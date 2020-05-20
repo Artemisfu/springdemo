5369eeac29adadb1e3de8d7cd6a5ccf4077309ad
Daisy <ouluoya@qq.com>
2020-05-20 17:48:04 +0800

    upload function.

---
+++ src/mvc/controller/UploadController.java
@@ -0,0 +1,21 @@
+package mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class UploadController {
+    @RequestMapping("/upload")
+    public void upload(@RequestParam("picture") MultipartFile picture) throws Exception {
+        System.out.println(picture.getOriginalFilename());
+    }
+
+    @RequestMapping("/test_upload")
+    public ModelAndView upload() {
+        return new ModelAndView("upload");
+    }
+}
